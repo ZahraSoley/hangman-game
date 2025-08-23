933801a87cf985ca0a264bf3d0de71d81fd15598
ZahraSoley <zahra.soleil.o@gmail.com>
2025-08-23 14:34:02 +0200

    Initial commit

---
+++ src/HangmanDrawing.tsx
@@ -0,0 +1,24 @@
+const HEAD = <div className="w-14 h-14 rounded-full border-solid border-black border-8 absolute right-[-33px] top-[47px]"></div>
+const BODY = <div className="h-25 w-2 bg-black absolute right-[-8px] top-[103px]"></div>
+const RIGHTARM = <div className="h-2 w-25 bg-black absolute right-[-108px] top-[133px] rotate-[-30deg] origin-bottom-left"></div>
+const LEFTARM = <div className="h-2 w-25 bg-black absolute right-[0px] top-[133px] rotate-[30deg] origin-bottom-right"></div>
+const LEFTLEG = <div className="h-2 w-25 bg-black absolute right-[-8px] top-[196px] rotate-[-60deg] origin-bottom-right"></div>
+const RIGHTLEG = <div className="h-2 w-25 bg-black absolute right-[-100px] top-[196px] rotate-[60deg] origin-bottom-left"></div>
+const HangmanDrawing = () => {
+    return (
+        <div className="relative ">
+            {HEAD}
+            {BODY}
+            {RIGHTARM}
+            {LEFTARM}
+            {RIGHTLEG}
+            {LEFTLEG}
+            <div className="w-2 h-12 bg-black absolute left-70"></div>
+            <div className="w-40 h-2 bg-black ml-30"></div>
+            <div className="h-100 w-2 bg-black ml-30"></div>
+            <div className="h-2 w-60 bg-black"></div>
+        </div>
+    )
+}
+
+export default HangmanDrawing
